df455cc7451a9db4fc3e0a8ead231959f8fef5e5
Magdalena Dyktyńska <magdalena.dyktyska@gmail.com>
2019-12-10 18:02:35 +0100

    first commit

--- src/app.js
+++ src/app.js
@@ -1,17 +1,25 @@
 import express from "express";
 import dotenv from "dotenv";
+import mongoose from "mongoose"
 import TestRestController from "./testModule/interface/TestRestController";
 import MockTestRepository from "./testModule/infrastructure/MockTestRepository";
 import TestService from "./testModule/core/TestService";
 
 dotenv.config();
+
 const app = express();
 const PORT = process.env.PORT || 5000;
+const db =  process.env.db;
 
 //initialization of the example testModule
 const testRepositoryImplementation = new MockTestRepository();
 const testServiceImplementation = new TestService(testRepositoryImplementation);
 const testApi = new TestRestController(testServiceImplementation);
+
+mongoose.connect(db)
+  .then(() => console.log('Connected to MongoDB...'))
+  .catch(err => console.error('Could not connect to MongoDB...'));
+
 
 //wiring up the routes
 app.use("/tests",testApi.router);
