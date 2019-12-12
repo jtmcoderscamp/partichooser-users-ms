03900acb3747d9165e4c78689581d0e951a7359e
Magdalena Dyktyńska <magdalena.dyktyska@gmail.com>
2019-12-12 20:01:51 +0100

    app

--- src/app.js
+++ src/app.js
@@ -31,5 +31,5 @@ mongoose.connect(db)
   .then(() => console.log('Connected to MongoDB...'))
   .catch(err => console.error('Could not connect to MongoDB...'));
 
-  const userRepository = new UserRepository();
+const userRepository = new UserRepository();
 userRepository.addUser('47','Anne','Connel','annc@gmail.com','mentor')
