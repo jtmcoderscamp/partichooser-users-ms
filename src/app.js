3ae7e3f078e5d4746c06d10340e1ba593d0738cf
jtmaluszynski <j.t.maluszynski@gmail.com>
2020-01-02 18:26:24 +0100

    capital letter

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ import dotenv from "dotenv";
 import mongoose from "mongoose";
 import UserRepository from "./userModule/infrastructure/UserRepository";
 import UserService from "./userModule/core/UserService";
-import UserRestController from "./userModule/interface/userRestController";
+import UserRestController from "./userModule/interface/UserRestController";
 
 /*dotenv.config();
 
